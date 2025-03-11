3f639b2046e325eab257371a4025dc372cc3f21e
Reipoh Gabriel Baldos Arradaza <reipohgabriel13@gmail.com>
2025-03-11 12:36:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from './components/forms/Form'
+import React, { useState } from 'react';
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+
+  return (
+    
+    <div >
+    <Form/>
+ 
+    </div>
+  );
+}
+
+export default App;
